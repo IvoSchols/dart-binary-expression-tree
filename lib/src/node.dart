3bb7428b5004c4bca16d8f178cd63ccde95d51f6
Ivo <ivoschols97@gmail.com>
2022-09-07 16:08:14 +0200

    added == operator

--- lib/src/node.dart
+++ lib/src/node.dart
@@ -26,12 +26,17 @@ class Node<T> {
 
   bool isLeaf() => left == null && right == null;
 
-  // Node is operator if is of type Char and contains: +, -, *, /, &&, ||, !
+  // Node is operator if is of type Char and contains: +, -, *, /, &&, ||, !, ==
   bool isOperator() =>
       value is String &&
-      const ['+', '-', '*', '/', '&&', '||', '!'].contains(value);
+      const ['+', '-', '*', '/', '&&', '||', '!', '=='].contains(value);
 
   // is numeric or boolean or 'variable'
   bool isOperand() =>
       value is num || value is bool || (value is String && !isOperator());
+
+  // To Dart interpretable string
+  String toDart() {
+    return 'Node($value)';
+  }
 }
